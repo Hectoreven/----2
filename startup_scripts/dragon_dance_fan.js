d3f9c31377cdd6615a31bc4178ff8889515768d5
chen_1335 <3388508783@qq.com>
2026-02-18 19:48:53 +0800

    回旋镖

--- startup_scripts/dragon_dance_fan.js
+++ startup_scripts/dragon_dance_fan.js
@@ -1,6 +1,9 @@
+var $EnergyBoomerangItem = Java.loadClass("cn.leolezury.eternalstarlight.common.item.combat.EnergyBoomerangItem")
+
+/** @type {typeof import("net.minecraft.world.item.Item$Properties").$Item$Properties } */
+var $Item$Properties  = Java.loadClass("net.minecraft.world.item.Item$Properties")
+
+
 StartupEvents.registry("item", event => {
-    event.create("dragon_dance_fan", "sword"). attackDamageBaseline(5)
-
-
-    })
-    
+    event.createCustom("dragon_dance_fan",()=> new $EnergyBoomerangItem(new $Item$Properties().stacksTo(1)))
+})
